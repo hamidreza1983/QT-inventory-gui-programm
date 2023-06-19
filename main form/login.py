b689ae76d86007b3a71babae0b7e541d132817b9
hamid reza <126154017+hamidreza1983@users.noreply.github.com>
2023-06-19 07:19:23 -0700

    create icon
    countinue basic form

--- main form/login.py
+++ main form/login.py
@@ -1,23 +1,28 @@
 from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QIcon
+import os
 import sys
 
 
 class Window(QWidget):
-    #def __init__(self):
-    #    super().__init__()
-    #    self.setUI()
+    def __init__(self):
+        super().__init__()
+        self.setUI()
 
     def setUI(self):
+        # -----------> Basic Window
         self.setWindowTitle('Test Window')
         self.setGeometry(300, 300, 300, 300)
         self.setMinimumSize(300, 300)
         self.setMaximumSize(300, 300)
+        self.setWindowIcon(QIcon(os.path.dirname(__file__) + "/icon.png"))
+        self.setStyleSheet("background-color: red;")
         #self.setFixedSize(300, 300)
-        #self.show()
+        #------------> QLable
+        self.show()
 
+        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     obj1 = Window()
-    obj1.setUI()
-    obj1.show()
     sys.exit(app.exec_())
