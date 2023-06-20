f13be9de90d10c52f1df8b6ea5473f7c6299b968
hamid reza <126154017+hamidreza1983@users.noreply.github.com>
2023-06-20 06:37:51 -0700

    training
    countinue

--- main form/login.py
+++ main form/login.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel
 from PyQt5.QtGui import QIcon
 import os
 import sys
@@ -11,18 +11,30 @@ class Window(QWidget):
 
     def setUI(self):
         # -----------> Basic Window
+
         self.setWindowTitle('Test Window')
-        self.setGeometry(300, 300, 300, 300)
-        self.setMinimumSize(300, 300)
-        self.setMaximumSize(300, 300)
-        self.setWindowIcon(QIcon(os.path.dirname(__file__) + "/icon.png"))
-        self.setStyleSheet("background-color: red;")
+        self.setGeometry(300, 300, 600, 600)
+        #self.setMinimumSize(300, 300)
+        #self.setMaximumSize(300, 300)
+        self.setWindowIcon(QIcon(os.path.dirname(__file__) + "/assets/icon.png"))
+        #self.setStyleSheet("background-color: red;")
         #self.setFixedSize(300, 300)
+
         #------------> QLable
+
         self.show()
+
+
+
+
+        
+        
+
+
+
 
         
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QApplication(sys.argv)    
     obj1 = Window()
     sys.exit(app.exec_())
